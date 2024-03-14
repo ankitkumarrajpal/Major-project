49286d933858a49e2c76793dca0574a67525cfbb
Ankit Kumar <153219449+ankitkumarrajpal@users.noreply.github.com>
2024-03-14 18:18:38 +0530

    Update post.js

--- classroom/routes/post.js
+++ classroom/routes/post.js
@@ -1,25 +1,3 @@
-const express = require("express");
-const router = express.Router();
 
 
-//posts
-router.get("/", (req,res) => {
-    res.send("GET for posts");
-});
-
-//show-posts
-router.get("/:id", (req,res) => {
-  res.send("GET for show posts")
-});
-
-//post-new post add
-router.post("/", (req,res) => {
-    res.send("post for posts")
-  });
-
-  //Delete-posts
-  router.delete("/:id", (req,res) => {
-    res.send("delete user")
-  }); 
-
-  module.exports = router;
+  
